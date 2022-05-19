399890c783442ab7b2c2b60c77f7c06526d7597c
osidekyle <hawkinskyle67@gmail.com>
2022-05-19 01:58:52 -0700

    allowed jwt to be used for auth

--- src/main/java/kyle/hawkins/springsecurityjwt/SecurityConfigurer.java
+++ src/main/java/kyle/hawkins/springsecurityjwt/SecurityConfigurer.java
@@ -1,8 +1,10 @@
 package kyle.hawkins.springsecurityjwt;
 
+import kyle.hawkins.springsecurityjwt.filters.JwtRequestFilter;
 import kyle.hawkins.springsecurityjwt.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,6 +19,9 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService myUserDetailsService;
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
@@ -26,11 +31,20 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll().
-                anyRequest().authenticated();
+                anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore((jwtRequestFilter, UsernamePasswordAuthenticationFilter.class));
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
